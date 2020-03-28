4e914e0b2455199ae93a6ecaccb255b42fa9ee73
Jose Munoz <jdmg93@gmail.com>
2020-03-28 12:22:59 -0700

    Updates

---
+++ precache-manifest.5cb536a3affe7d464f484d206af72e36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7903af16f899ef925b1b892abf3005",
+    "url": "/fast-trivia/index.html"
+  },
+  {
+    "revision": "87e0be4a9fa596f9d0bc",
+    "url": "/fast-trivia/static/js/2.cfa9adb3.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/fast-trivia/static/js/2.cfa9adb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc05b1e18f76616bca3e",
+    "url": "/fast-trivia/static/js/main.f140a61e.chunk.js"
+  },
+  {
+    "revision": "6addf76f75e6598576f5",
+    "url": "/fast-trivia/static/js/runtime-main.cae75c03.js"
+  }
+]);
